b255ff574c81d145588f44f450f37a8d5b0e0f1d
Swallowedin <charles.thomasse@gmail.com>
2024-12-19 16:14:01 +0100

    Update carto.py

--- carto.py
+++ carto.py
@@ -215,7 +215,9 @@ with tab1:
                     </span>
                 </div>
             """, unsafe_allow_html=True)
-
+	    if st.button("", key=f"add_risk_{family_key}", help="Ajouter un risque"):
+                st.session_state[f"show_risk_form_{family_key}"] = True
+		
             # Formulaire d'ajout de risque
             if st.session_state.get(f"show_risk_form_{family_key}", False):
                 with st.form(key=f"risk_form_{family_key}"):
